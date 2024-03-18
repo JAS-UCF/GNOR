42c708f55067b6cd141021a186af4ed3d2cffee6
BiggiePete <pter.cross222@gmail.com>
2024-03-18 19:06:47 -0400

    pinout

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+
+#define MOTOR_LEFT 27
+#define MOTOR_RIGHT 26
 
 void setup()
 {
-  // put your setup code here, to run once:
+  // set pins to output
+  pinMode(MOTOR_LEFT, OUTPUT);  // init left
+  pinMode(MOTOR_RIGHT, OUTPUT); // init right
+
+  // Adjust PWM properties if needed
+  /*
+  analogWriteFreq(5000);
+  analogWriteRange(65535);
+  analogWriteResolution(16);
+  */
 }
 
 void loop()
